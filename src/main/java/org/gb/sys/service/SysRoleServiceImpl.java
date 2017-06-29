3e753b678c8a4eb45737143c9713e2af3c145fd2
zyl2286375311 <2286375311@qq.com>
2017-06-29 11:25:17 +0800

    ROLE

--- src/main/java/org/gb/sys/service/SysRoleServiceImpl.java
+++ src/main/java/org/gb/sys/service/SysRoleServiceImpl.java
@@ -19,7 +19,8 @@ public class SysRoleServiceImpl implements SysRoleService {
 
 	@Autowired
 	private SysRoleDao sysRoleDao;
-@Override
+
+	@Override
     public PageUtil<SysRole> selectRoleList(PageUtil<SysRole> rolePage) {
     	return null;
     }
@@ -43,13 +44,4 @@ public class SysRoleServiceImpl implements SysRoleService {
 		});
 		return roleList;
 	}
-
-	@Override
-	public PageUtil<SysRole> selectRoleList(PageUtil<SysRole> rolePage) {
-		return sysRoleDao.selectRoleList(rolePage);
-	}
-
-
-
-
 }
